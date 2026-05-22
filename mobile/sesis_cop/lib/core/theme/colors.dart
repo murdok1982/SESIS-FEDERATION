84d46f660750275b91eaf62330e6b12b909c6339
Gustavo <gustavolobatoclara@gmail.com>
2026-05-22 21:26:35 +0200

    feat(mobile): create colors.dart

---
+++ mobile/sesis_cop/lib/core/theme/colors.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class MilitaryColors {
+  MilitaryColors._();
+
+  static const Color background = Color(0xFF0A0E17);
+  static const Color surface = Color(0xFF131926);
+  static const Color surfaceLight = Color(0xFF1C2438);
+  static const Color primary = Color(0xFF3DDC84);
+  static const Color primaryDark = Color(0xFF2BA864);
+  static const Color error = Color(0xFFE94560);
+  static const Color warning = Color(0xFFFFB74D);
+  static const Color info = Color(0xFF4FC3F7);
+  static const Color success = Color(0xFF3DDC84);
+
+  static const Color textPrimary = Color(0xFFE8EAED);
+  static const Color textSecondary = Color(0xFF9AA0A6);
+  static const Color textMuted = Color(0xFF5F6368);
+
+  static const Color classificationRestricted = Color(0xFF1B5E20);
+  static const Color classificationConfidential = Color(0xFF0D47A1);
+  static const Color classificationSecret = Color(0xFFB71C1C);
+  static const Color classificationTopSecret = Color(0xFF880E4F);
+  static const Color classificationCosmic = Color(0xFF4A148C);
+
+  static const Color mapLand = Color(0xFF1A1F2E);
+  static const Color mapWater = Color(0xFF0D1B2A);
+  static const Color mapGrid = Color(0xFF2A3A4A);
+
+  static const Color ghostMode = Color(0xFF455A64);
+  static const Color duress = Color(0xFFD32F2F);
+
+  static const List<Color> statusGradient = [
+    Color(0xFF3DDC84),
+    Color(0xFFFFB74D),
+    Color(0xFFE94560),
+  ];
+}
