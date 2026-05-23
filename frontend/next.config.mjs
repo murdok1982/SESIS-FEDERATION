6ac64b1e69ee2c83f1184e541cecb21152e327b9
Gustavo <gustavolobatoclara@gmail.com>
2026-05-23 06:57:20 +0200

    feat: update frontend/next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
   output: "standalone",
   async rewrites() {
     return [
-      { source: "/api/:path*", destination: `${process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000/api/v1"}/:path*` },
+      { source: "/api/:path*", destination: process.env.NEXT_PUBLIC_API_URL + "/:path*" },
     ];
   },
 };
